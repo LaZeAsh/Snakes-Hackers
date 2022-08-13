2ef5cd3da991e382800411f1cc741fa2ad57c8df
KlausMikhaelson <satyamsingh5076@gmail.com>
2022-08-14 02:15:41 +0530

    added chats

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
-const express = require('express');
-const app = express();
+const express = require("express");
+const app = express()
+// const PORT = process.env.PORT || 3001;
+const server = require("http").createServer(app)
+const io = require("socket.io")(server, { cors: { origin: "*" }})
 
-app.set("view engine", "ejs");
+
+app.set("view engine", "ejs")
+app.use(express.static(__dirname + '/public'));
+console.log("Yup worked")
+const users = {}
+
 
 app.get("/", (req, res) => {
-  res.render("index")
+    res.render("index")
 })
 
-app.listen(process.env.PORT || 3000)
+server.listen(process.env.PORT || 3001, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
+
+io.on("connection", function(socket) {
+    
+    console.log("user conncted" + socket.id);
+})
+
+io.on('connection', socket => {
+    socket.on('new-user', name => {
+      users[socket.id] = name
+      socket.broadcast.emit('user-connected', name)
+    })
+    socket.on('send-chat-message', message => {
+      socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
+    })
+    socket.on('disconnect', () => {
+      socket.broadcast.emit('user-disconnected', users[socket.id])
+      delete users[socket.id]
+    })
+})
