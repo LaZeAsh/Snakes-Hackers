a4fe37355aa956d3e37276b50668abdf74b5c447
KlausMikhaelson <satyamsingh5076@gmail.com>
2022-08-14 11:57:37 +0530

    mvp is ready

--- index.js
+++ index.js
@@ -66,6 +66,19 @@ io.on("connection",(socket)=>{
     });
 })
 
+io.on('connection', socket => {
+    socket.on('new-user', name => {
+      users[socket.id] = name
+      socket.broadcast.emit('user-connected', name)
+    })
+    socket.on('send-chat-message', message => {
+      socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
+    })
+    socket.on('disconnect', () => {
+      socket.broadcast.emit('user-disconnected', users[socket.id])
+      delete users[socket.id]
+    })})
+
 //Function to calculate winner
 const getWinner=(p, c)=>  {
     if (p === c) {
